b2d90c5620b063eb84f68ac497925b541f76fadc
Mridul Mayank <mridul063@gmail.com>
2020-08-22 18:03:28 +0530

    min and max of array in recursive

--- src/com/mridul/algo/SumOfArrayUsingRecursive.java
+++ src/com/mridul/algo/SumOfArrayUsingRecursive.java
@@ -6,13 +6,31 @@ public class SumOfArrayUsingRecursive {
 		int array[] = { 1, 2, 3, 4 };
 		SumOfArrayUsingRecursive obj = new SumOfArrayUsingRecursive();
 		System.out.println(obj.sum(array, array.length));
+		System.out.println(obj.findMax(array, array.length));
+		System.out.println(obj.findMin(array, array.length));
 	}
 
 	public int sum(int[] array, int length) {
 		if (length <= 0) {
 			return 0;
 		} else {
-			return sum(array, length - 1) + array[length-1];
+			return array[length - 1] + sum(array, length - 1);
+		}
+	}
+
+	public int findMax(int[] array, int length) {
+		if (length == 1) {
+			return array[0];
+		} else {
+			return Math.max(array[length - 1], findMax(array, length - 1));
+		}
+	}
+	
+	public int findMin(int[] array, int length) {
+		if (length == 1) {
+			return array[0];
+		} else {
+			return Math.min(array[length - 1], findMin(array, length - 1));
 		}
 	}
 
